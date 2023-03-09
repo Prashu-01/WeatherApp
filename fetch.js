a067d8538e44bf8f24bbe7a860ee7387123417af
Prashu Verma <prashuverma012@gmail.com>
2023-03-09 15:53:56 +0530

    Imporvement

--- fetch.js
+++ fetch.js
@@ -1,7 +1,36 @@
 // setting default city from cookies
-let city = "Varanasi"
 
-// date on weather app
+function Get_Cookie( check_name ) {
+    var all_cookies = document.cookie.split( ';' );
+    var temp_cookie = '';
+    var cookie_name = '';
+    var cookie_value = '';
+    var b_cookie_found = false;
+ 
+    for ( i = 0; i < all_cookies.length; i++ )
+    {
+        temp_cookie = all_cookies[i].split( '=' );
+        cookie_name = temp_cookie[0].replace(/^\s+|\s+$/g, '');
+        if ( cookie_name == check_name )
+        {
+            b_cookie_found = true;
+            if ( temp_cookie.length > 1 )
+            {
+                cookie_value = unescape( temp_cookie[1].replace(/^\s+|\s+$/g, '') );
+            }
+            return cookie_value;
+            break;
+        }
+        temp_cookie = null;
+        cookie_name = '';
+    }
+    if ( !b_cookie_found )
+    {
+        return null;
+    }
+}
+
+// date-month on weather app
 let d = new Date()
 let mon = d.getMonth()
 let str_mon
@@ -47,11 +76,14 @@ document.querySelector(".date").innerHTML= d.getDate()
 document.querySelector(".mon").innerHTML = str_mon
 
 // displaying weather of searched city
-function myfun() {
+function fetch_func() {
     let city = document.querySelector("#search-city").value
+    if(city=='') city=Get_Cookie('city3');
     let url = `https://api.weatherapi.com/v1/current.json?key=ae8808174ead4af190b94146221112&q=${city}`
     let allcard = document.querySelector("#temp-details")
+    
     let response = fetch(url)
+    
     response.then((v) => {
         return v.json()
     }).then((weather) => {
@@ -62,6 +94,7 @@ function myfun() {
             console.log(weather[items])
         }
         
+        // updating name of city and temperature
         document.querySelector("#temp").innerHTML = `${weather[items].temp_c} &deg`
         document.querySelector(".c-name").innerHTML = `${city}`
         
@@ -76,7 +109,7 @@ function myfun() {
         let bg=document.querySelector("#container")
         switch(status){
             case "Sunny":
-                bg.style.background="url(img/sunny_day.jpg) no-repeat fixed center"
+                bg.style.background="url(img/desert.jpg) no-repeat fixed center"
                 document.querySelector(".whether-status").innerHTML=`<img src="img/weather-icons/sun.png" alt="icon" class="whether-icon">
                 <div class="wea-status">${status}</div>`
                 break
@@ -114,7 +147,7 @@ function myfun() {
                 break
         }
 
-        // more weather status div
+        // more weather conditions div
         cardhtml =
           `<span class="temp-d">Temp: ${weather[items].temp_c} &deg;C</span>
           <span class="temp-d">Feels like: ${weather[items].feelslike_c} &deg;C</span>
@@ -128,5 +161,5 @@ function myfun() {
           <span class="temp-d">Pressure-in: ${weather[items].pressure_in}</span>
           <span class="temp-d">UV: ${weather[items].uv} mW/m &square;</span>`
         allcard.innerHTML = cardhtml
-    }).catch(err => alert(err))
+    }).catch(err => alert(err)) //to handle the remaining errors
 }
